49a4a2e204d1f2d3de357855fd17ec375aaef745
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-11-02 16:27:50 +0100

    Create Database.php

---
+++ app/models/Database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = 'mariadb';
+    private $db_name = 'k0munitat';
+    private $username = 'root';
+    private $password = 'rootpwd';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
