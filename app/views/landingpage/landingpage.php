7742e4612596e605da8420ed31eef61bfb04ae45
ncastell <nataliacastell@iesmontsia.org>
2023-11-13 22:58:10 +0100

    add blocs

--- app/views/landingpage/landingpage.php
+++ app/views/landingpage/landingpage.php
@@ -1,10 +1,9 @@
 <?php
 
 include('../header/header.php');
+include('inici.php');
+include('proyecto.php');
+include('mapa.php');
 include('nosotros.php');
-
-
-include('../footer/footer.php');
-
 
 ?>
