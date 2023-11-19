4483144ceec228ae927fed75501c448da58aec77
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2023-11-19 16:34:24 +0100

    login controller POO

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,36 +1,53 @@
 <?php
     include("../models/User.php"); 
-if (isset($_POST['email'])){
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-        $email = htmlspecialchars($_POST['email']);
-        $password = htmlspecialchars($_POST['pass']);
-        $password_md5 = md5($password);
-        $user = new User();
+class LoginController {
+    private $user;
 
-        if($user->login($email, $password_md5)){
-            //las credenciales son correctas, iniciar sesion y redirigir al usuario
-            session_start(); 
+    public function __construct() {
+        $this->user = new User();
+    }
 
-            //almacena informacion del usuario en la sesion
-            $_SESSION['id_user'] = $user->getId();
-            $_SESSION['email'] = $user->getEmail();
-            $_SESSION['role'] = $user->getRole();
-            $_SESSION['username'] = $user->getUsername();
-
-            //redirige al usuario
-            header('Location: ../index.php');
-
-            exit;
-        }else{
-            //las credenciales no son correctas
-            echo "Ingrese un usuario y contraseña correctos";
+    public function processLogin($email, $password_md5) {
+        if ($this->user->login($email, $password_md5)) {
+            $this->startSession();
+            $this->redirectUser();
+        } else {
+            $this->displayErrorMessage("Ingrese un usuario y contraseña correctos");
         }
-    }else{
-            echo "Error en la conexión a la base de datos" ;
+    }
+
+    //almacena información del usuario
+    private function startSession() {
+        session_start();
+        $_SESSION['user_id'] = $this->user->getId();
+        $_SESSION['email'] = $this->user->getEmail();
+        $_SESSION['role'] = $this->user->getRole();
+        $_SESSION['username'] = $this->user->getUsername();
+    }
+
+    private function redirectUser() {
+        header("Location: ../views/landingpage/landingpage.php");
+        exit;
+    }
+
+    private function displayErrorMessage($message) {
+        echo $message;
     }
 }
-    
-    
 
+
+if(isset($_POST['email'])){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['pass']);
+    $password_md5 = md5($password);
+
+    //creo objeto de Login controller que crea un objeto basado en la clase usuario
+    $loginController = new LoginController();
+    $loginController->processLogin($email, $password_md5);
+} else {
+    echo "Error en la conexión de la base de datos";
+}
+}
 ?>
