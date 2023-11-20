507a15cedc1deee42e8bdc8f1707735f28e73518
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2023-11-20 15:26:09 +0100

    error de community

--- app/views/community.php
+++ app/views/community.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-<?php include("./header/header.php");?>
+ include("./header/header.php");
 if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
 ?>
 <!DOCTYPE html>
